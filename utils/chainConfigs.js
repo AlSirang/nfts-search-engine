7a41d345ed1b0207b9a0e06fd697ebe6ba92e50d
AlSirang <uddinbashir1999@gmail.com>
2022-11-08 01:59:09 +0500

    fix: add chain configs

--- utils/chainConfigs.js
+++ utils/chainConfigs.js
@@ -20,3 +20,26 @@ export const constChainConfigs = [
     chainId: "0xfa",
   },
 ];
+
+export const chainIdToURL = {
+  "0x1": {
+    rpcURL: "https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+    blockExplorer: "https://etherscan.io",
+  },
+  "0x89": {
+    rpcURL: "https://polygon-rpc.com",
+    blockExplorer: "https://polygonscan.com",
+  },
+  "0x38": {
+    rpcURL: "https://bsc-dataseed.binance.org",
+    blockExplorer: "https://bscscan.com",
+  },
+  "0xa86a": {
+    rpcURL: "https://api.avax.network/ext/bc/C/rpc",
+    blockExplorer: "https://snowtrace.io",
+  },
+  "0xfa": {
+    rpcURL: "https://rpc.ftm.tools/",
+    blockExplorer: "https://ftmscan.com",
+  },
+};
