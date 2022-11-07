ab8b36045c9a8eeba807f1a6ad41c7f2af660dac
AlSirang <uddinbashir1999@gmail.com>
2022-11-08 02:01:30 +0500

    fix: filter contract tyep + update payload

--- components/Section.js
+++ components/Section.js
@@ -4,23 +4,39 @@ import { useMoralis } from "../hooks/useMoralis";
 
 import styles from "../styles/Section.module.css";
 
-const renderCards = (data) => {
+const renderCards = (data, chainId) => {
   const { result } = data;
 
-  let nftsMetaData = result.map(({ metadata }) => {
-    const _metadata = JSON.parse(metadata);
+  let nftsMetaData = result.map(
+    ({ metadata, contract_type, token_address, token_id }) => {
+      const _metadata = JSON.parse(metadata);
 
-    return _metadata;
-  });
+      return {
+        metadata: _metadata,
+        contractType: contract_type,
+        contract: token_address,
+        tokenId: token_id,
+      };
+    }
+  );
 
-  nftsMetaData = nftsMetaData.filter((metadata) => Boolean(metadata));
+  nftsMetaData = nftsMetaData.filter(
+    ({ contractType }) => contractType === "ERC721"
+  );
   if (!nftsMetaData.length) {
     return <h4>No NFTs found</h4>;
   }
   return (
     <>
-      {nftsMetaData.map(({ name, image }) => (
-        <Card key={name} name={name} image={image} />
+      {nftsMetaData.map(({ metadata: { name, image }, contract, tokenId }) => (
+        <Card
+          key={contract + tokenId}
+          name={name}
+          image={image}
+          contract={contract}
+          tokenId={tokenId}
+          chainId={chainId}
+        />
       ))}
     </>
   );
@@ -35,7 +51,7 @@ export default React.memo(function Section({ chainId, chainName }) {
       <h3>{chainName}</h3>
       <hr />
       <div className={styles.cardsContainer}>
-        {!isLoading && success && renderCards(response)}
+        {!isLoading && success && renderCards(response, chainId)}
         {!isLoading && failure && <p>{error.message || error}</p>}
         {isLoading && <h3>Loading...</h3>}
       </div>
