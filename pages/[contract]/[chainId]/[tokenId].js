dfa57492ac57c48256faab34db5fe003b19e6f10
AlSirang <uddinbashir1999@gmail.com>
2022-11-10 23:01:09 +0500

    fix: add traverse form

--- pages/[contract]/[chainId]/[tokenId].js
+++ pages/[contract]/[chainId]/[tokenId].js
@@ -2,77 +2,119 @@ import Web3 from "web3";
 import axios from "axios";
 import { Multicall } from "ethereum-multicall";
 import clsx from "clsx";
-import { chainIdToURL } from "../../../utils/chainConfigs";
+import { chainConfigs, chainIdToInfo } from "../../../utils/chainConfigs";
 import ABI_ERC721 from "../../../assets/abis/basicERC721.abi.json";
 import { getNavigableURL, shortenAddress } from "../../../utils/constants";
 import styles from "../../../styles/WalletInfo.module.css";
+import Meta from "../../../components/Meta";
 
 export default function Page({ nftInfo }) {
-  const {
-    metadata,
-    name: contractName,
-    symbol,
-    contractAddress,
-    tokenId,
-    chainId,
-  } = nftInfo;
+  const { metadata, contractAddress, tokenId, chainId, metadataURI } = nftInfo;
   const { image, name, description } = metadata;
 
-  const blockExplorer = chainIdToURL[chainId].blockExplorer;
+  const blockExplorer = chainIdToInfo[chainId].blockExplorer;
 
   let imgSrc = getNavigableURL(image);
 
   return (
-    <section className={clsx("container", styles.container)}>
-      <div className="row">
-        <div className="col-md-5">
-          <picture>
-            <video
-              poster={imgSrc}
-              // className={styles.cardImage}
-              src={imgSrc}
-              alt="Card image cap"
-              width="100%"
-              autoPlay
-              playsInline
-              loop
-            />
-          </picture>
-        </div>
+    <section className="pb-5">
+      <Meta title={name} description={description} />
+      <div className={clsx("container", styles.container)}>
+        <div className="row">
+          <div className="col-md-5">
+            <picture>
+              <video
+                poster={imgSrc}
+                // className={styles.cardImage}
+                src={imgSrc}
+                alt="Card image cap"
+                width="100%"
+                autoPlay
+                playsInline
+                loop
+              />
+            </picture>
+          </div>
 
-        <div className="col-md-1" />
+          <div className="col-md-1" />
 
-        <div className="col-md-6">
-          <h1>{name}</h1>
-          <h5>Description</h5>
-          <p>{description}</p>
-          <hr />
+          <div className="col-md-6">
+            <h1>{name}</h1>
+            <h5>Description</h5>
+            <p>{description}</p>
+            <hr />
 
-          <h5>Name: {contractName} </h5>
-          <h5>Symbol: {symbol} </h5>
+            <div className="row">
+              {/* left start*/}
+              <div className="col-6 h5-mb0">
+                <div>
+                  <h5>Source Chain</h5>
+                  <p>{chainIdToInfo[chainId].chainName}</p>
+                </div>
 
-          <h5>
-            Token Id:&nbsp;
-            <a
-              className="hover-color"
-              href={`${blockExplorer}/token/${contractAddress}?a=${tokenId}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {tokenId}
-            </a>
-          </h5>
-          <h5>
-            Address:&nbsp;
-            <a
-              className="hover-color"
-              href={`${blockExplorer}/token/${contractAddress}`}
-              target="_blank"
-              rel="noreferrer"
-            >
-              {shortenAddress(contractAddress)}
-            </a>
-          </h5>
+                <div>
+                  <h5>Select Destination Chain</h5>
+                  <div className="p-2" />
+                  <select className="form-select">
+                    {chainConfigs
+                      .filter(({ chainId: _chainId }) => _chainId !== chainId)
+                      .map(({ chainName, chainId }) => (
+                        <option key={chainId}>{chainName}</option>
+                      ))}
+                  </select>
+                </div>
+
+                <div className="mt-4">
+                  <button className="btn btn-outline-secondary w-100">
+                    Traverse
+                  </button>
+                </div>
+              </div>
+              {/* left end*/}
+
+              {/* right  start*/}
+              <div className="col-6 h5-mb0">
+                <h5>Source Contract</h5>
+                <p>
+                  <a
+                    className="hover-color"
+                    href={`${blockExplorer}/token/${contractAddress}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {shortenAddress(contractAddress)}
+                  </a>
+                </p>
+
+                <h5>NFT Name</h5>
+                <p>{name}</p>
+
+                <h5>NFT Id</h5>
+                <p>
+                  <a
+                    className="hover-color"
+                    href={`${blockExplorer}/token/${contractAddress}?a=${tokenId}`}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    {tokenId}
+                  </a>
+                </p>
+
+                <p>
+                  <a
+                    className="hover-color"
+                    href={metadataURI}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Show metadata
+                  </a>
+                </p>
+                {/* right end*/}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
@@ -82,7 +124,7 @@ export default function Page({ nftInfo }) {
 export const getServerSideProps = async (context) => {
   const { contract, tokenId, chainId } = context.params;
 
-  const rpcURL = chainIdToURL[chainId].rpcURL;
+  const rpcURL = chainIdToInfo[chainId].rpcURL;
 
   const web3 = new Web3(rpcURL);
 
@@ -129,6 +171,7 @@ export const getServerSideProps = async (context) => {
       contractAddress: contract,
       tokenId,
       chainId,
+      metadataURI: toFetchURI,
     };
 
     return {
