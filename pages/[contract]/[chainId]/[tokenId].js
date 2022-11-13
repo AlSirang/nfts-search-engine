d3099b6dbce850f63ec3396a90c30d7af9002322
AlSirang <uddinbashir1999@gmail.com>
2022-11-13 21:49:26 +0500

    feat: add switch network on traverse page

--- pages/[contract]/[chainId]/[tokenId].js
+++ pages/[contract]/[chainId]/[tokenId].js
@@ -6,118 +6,157 @@ import ABI_ERC721 from "../../../assets/abis/basicERC721.abi.json";
 import { getNavigableURL, shortenAddress } from "../../../utils/constants";
 import Meta from "../../../components/Meta";
 import WalletInfo from "../../../components/WalletInfo";
+import { Web3UserContext } from "../../../context";
+import { switchNetwork } from "../../../context/utils";
 
-export default function Page({ nftInfo }) {
-  const { metadata, contractAddress, tokenId, chainId, metadataURI } = nftInfo;
+export default function Page({ nftInfo, isInfoLoaded, reason }) {
+  const {
+    metadata,
+    contractAddress,
+    tokenId,
+    chainId,
+    metadataURI,
+    tokenOwner,
+  } = nftInfo;
   const { image, name, description } = metadata;
 
   const blockExplorer = chainIdToInfo[chainId].blockExplorer;
 
   let imgSrc = getNavigableURL(image);
 
+  const {
+    contextState: { account, provider, connectedChainId, isWalletConnected },
+  } = Web3UserContext();
+
+  const isSameChain = parseInt(connectedChainId) === parseInt(chainId);
+  const isOwner =
+    String(tokenOwner).toLocaleLowerCase() ===
+    String(account).toLocaleLowerCase();
+  const sourceChain = chainIdToInfo[chainId].chainName;
   return (
     <>
       <Meta title={name} description={description} />
       <section className="pb-5">
-        <WalletInfo />
+        <div className="pb-3">
+          <WalletInfo />
+        </div>
+
         <div className="container mt-5">
-          <hr />
-          <div className="row mt-5">
-            <div className="col-md-5">
-              <picture>
-                <video
-                  poster={imgSrc}
-                  // className={styles.cardImage}
-                  src={imgSrc}
-                  alt="Card image cap"
-                  width="100%"
-                  autoPlay
-                  playsInline
-                  loop
-                />
-              </picture>
-            </div>
+          {!isInfoLoaded && <h2>ERROR#500: {reason}</h2>}
 
-            <div className="col-md-1" />
+          {isInfoLoaded && (
+            <div className="row mt-5">
+              <div className="col-md-5">
+                <picture>
+                  <video
+                    poster={imgSrc}
+                    src={imgSrc}
+                    alt="Card image cap"
+                    width="100%"
+                    autoPlay
+                    playsInline
+                    loop
+                  />
+                </picture>
+              </div>
 
-            <div className="col-md-6">
-              <h1>{name}</h1>
-              <h5>Description</h5>
-              <p>{description}</p>
-              <hr />
+              <div className="col-md-1" />
 
-              <div className="row">
-                {/* left start*/}
-                <div className="col-6 h5-mb0">
-                  <div>
-                    <h5>Source Chain</h5>
-                    <p>{chainIdToInfo[chainId].chainName}</p>
+              <div className="col-md-6">
+                <h1>{name}</h1>
+                <h5>Description</h5>
+                <p>{description}</p>
+                <hr />
+
+                <div className="row">
+                  {/* left start*/}
+
+                  <div className="col-6 h5-mb0">
+                    <div>
+                      <h5>Source Chain</h5>
+                      <p>{sourceChain}</p>
+                    </div>
+
+                    <div>
+                      <h5>Select Destination Chain</h5>
+                      <div className="p-2" />
+                      <select className="form-select">
+                        {chainConfigs
+                          .filter(
+                            ({ chainId: _chainId }) => _chainId !== chainId
+                          )
+                          .map(({ chainName, chainId }) => (
+                            <option key={chainId}>{chainName}</option>
+                          ))}
+                      </select>
+                    </div>
+
+                    <div className="mt-4">
+                      {isSameChain && isOwner && (
+                        <button className=" example-btn w-100">Traverse</button>
+                      )}
+
+                      {isWalletConnected && !isSameChain && (
+                        <button
+                          className="btn btn-outline-secondary button-round w-100"
+                          onClick={switchNetwork.bind(this, provider, chainId)}
+                        >
+                          Switch Chain to {sourceChain}
+                        </button>
+                      )}
+
+                      {!isWalletConnected && (
+                        <h5>Please connect your wallet</h5>
+                      )}
+                    </div>
                   </div>
+                  {/* left end*/}
 
-                  <div>
-                    <h5>Select Destination Chain</h5>
-                    <div className="p-2" />
-                    <select className="form-select">
-                      {chainConfigs
-                        .filter(({ chainId: _chainId }) => _chainId !== chainId)
-                        .map(({ chainName, chainId }) => (
-                          <option key={chainId}>{chainName}</option>
-                        ))}
-                    </select>
+                  {/* right  start*/}
+                  <div className="col-6 h5-mb0">
+                    <h5>Source Contract</h5>
+                    <p>
+                      <a
+                        className="hover-color"
+                        href={`${blockExplorer}/token/${contractAddress}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {shortenAddress(contractAddress)}
+                      </a>
+                    </p>
+
+                    <h5>NFT Name</h5>
+                    <p>{name}</p>
+
+                    <h5>NFT Id</h5>
+                    <p>
+                      <a
+                        className="hover-color"
+                        href={`${blockExplorer}/token/${contractAddress}?a=${tokenId}`}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        {tokenId}
+                      </a>
+                    </p>
+
+                    <p>
+                      <a
+                        className="hover-color"
+                        href={metadataURI}
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Show metadata
+                      </a>
+                    </p>
+                    {/* right end*/}
                   </div>
-
-                  <div className="mt-4">
-                    <button className="btn btn-outline-secondary w-100">
-                      Traverse
-                    </button>
-                  </div>
-                </div>
-                {/* left end*/}
-
-                {/* right  start*/}
-                <div className="col-6 h5-mb0">
-                  <h5>Source Contract</h5>
-                  <p>
-                    <a
-                      className="hover-color"
-                      href={`${blockExplorer}/token/${contractAddress}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {shortenAddress(contractAddress)}
-                    </a>
-                  </p>
-
-                  <h5>NFT Name</h5>
-                  <p>{name}</p>
-
-                  <h5>NFT Id</h5>
-                  <p>
-                    <a
-                      className="hover-color"
-                      href={`${blockExplorer}/token/${contractAddress}?a=${tokenId}`}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      {tokenId}
-                    </a>
-                  </p>
-
-                  <p>
-                    <a
-                      className="hover-color"
-                      href={metadataURI}
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Show metadata
-                    </a>
-                  </p>
-                  {/* right end*/}
                 </div>
               </div>
             </div>
-          </div>
+          )}
         </div>
       </section>
     </>
@@ -154,6 +193,11 @@ export const getServerSideProps = async (context) => {
           methodName: "tokenURI",
           methodParameters: [tokenId],
         },
+        {
+          reference: "ERC721",
+          methodName: "ownerOf",
+          methodParameters: [tokenId],
+        },
       ],
     },
   ];
@@ -175,14 +219,29 @@ export const getServerSideProps = async (context) => {
       tokenId,
       chainId,
       metadataURI: toFetchURI,
+      tokenOwner: res.results.ERC721.callsReturnContext[3].returnValues[0],
     };
 
     return {
-      props: { nftInfo, isInfoLoaded: true },
+      props: { nftInfo, isInfoLoaded: true, reason: null },
     };
   } catch (err) {
     return {
-      props: { nftInfo: {}, isInfoLoaded: false },
+      props: {
+        nftInfo: {
+          name: null,
+          symbol: null,
+          metadata: {},
+          contractAddress: contract,
+          tokenId,
+          chainId,
+          metadataURI: null,
+          tokenOwner: null,
+        },
+        isInfoLoaded: false,
+
+        reason: err.message || "Something went wrong loading nft data.",
+      },
     };
   }
 };
