cd22b37aa520b4e5c5b24c182f072f34d75151f5
AlSirang <uddinbashir1999@gmail.com>
2022-11-22 14:00:32 +0500

    fix: add apis

--- services/auth.js
+++ services/auth.js
@@ -9,7 +9,19 @@ export const getSignInAPI = async (payload) => {
 export const checkTokenAPI = async (accessToken) => {
   return await axios({
     method: "get",
-    url: `${APIs_BASE_URL}/api/checkToken`,
-    headers: accessToken ? { cookie: accessToken } : undefined,
+    url: `${APIs_BASE_URL}/api/check-token`,
+    headers: accessToken
+      ? { cookie: `dynaswapToken=${accessToken}` }
+      : undefined,
+  });
+};
+
+export const addContractAPI = async (payload) => {
+  return await axios.post(`${APIs_BASE_URL}/api/add-contract`, payload);
+};
+
+export const getContractsAPI = async (page = 0) => {
+  return await axios.get(`${APIs_BASE_URL}/api/get-contracts`, {
+    params: { page },
   });
 };
