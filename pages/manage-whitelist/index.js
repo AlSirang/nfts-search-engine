90a0feb458bd10f9d2115e009a3842777824424a
AlSirang <uddinbashir1999@gmail.com>
2022-11-22 14:00:48 +0500

    feat: render contract info

--- pages/manage-whitelist/index.js
+++ pages/manage-whitelist/index.js
@@ -1,15 +1,37 @@
 import { DashboardNav } from "../../components/DashboardNav";
 import { AuthLayout } from "../../components/AuthLayout";
 import { getAppCookies } from "../../middlewares/utils";
-import { checkTokenAPI } from "../../services/auth";
+import { checkTokenAPI, getContractsAPI } from "../../services/auth";
+import { useEffect, useRef, useState } from "react";
+import ContractInfoCard from "../../components/ContractInfoCard";
 
 export default function Index(props) {
   const { isAuthenticated } = props;
+  const isEffectHookInit = useRef(false);
 
+  const [{ contractsInfo, limit, page, totalPages }, setContractsInfo] =
+    useState({ contractsInfo: [], limit: null, page: null, totalPages: null });
+
+  useEffect(() => {
+    if (isEffectHookInit.current) return;
+
+    isAuthenticated &&
+      getContractsAPI()
+        .then((res) => setContractsInfo({ ...res.data }))
+        .catch(console.log);
+
+    isEffectHookInit.current = true;
+  }, [isAuthenticated]);
   return (
     <AuthLayout isAuthenticated={isAuthenticated}>
       <section className="body-box">
         <DashboardNav />
+
+        <>
+          {contractsInfo.map((contractInfo) => (
+            <ContractInfoCard {...contractInfo} key={contractInfo._id} />
+          ))}
+        </>
       </section>
     </AuthLayout>
   );
