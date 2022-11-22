299818f2a4de7664a4b49ec1c9903c63f24d7c8f
AlSirang <uddinbashir1999@gmail.com>
2022-11-22 12:54:45 +0500

    fix: update auth midddleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,6 +1,6 @@
 import createHttpError from "http-errors";
 import User from "../models/User";
-import { verifyToken } from "./utils";
+import { getAppCookies, verifyToken } from "./utils";
 
 /**
  * @dev custom middleware for authenticating users
@@ -10,14 +10,11 @@ import { verifyToken } from "./utils";
  */
 export const authenticate = async (req, res, next) => {
   try {
-    const jwtToken = req.headers.cookie;
-
-    if (!jwtToken) return res.sendStatus(401);
-    const tokenDecode = verifyToken(jwtToken.split(" ")[1]);
-
+    const { dynaswapToken } = getAppCookies(req);
+    if (!dynaswapToken) return res.status(401).send("unauthorized");
+    const tokenDecode = verifyToken(dynaswapToken.split(" ")[1]);
     const user = await User.findOne({ _id: tokenDecode.sub });
     req.user = user;
-
     next();
   } catch (err) {
     next(createHttpError(500, err.message || err));
