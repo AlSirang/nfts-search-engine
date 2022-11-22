1d36a6c0109aa7f4f8c3b3593841c35010eb466c
AlSirang <uddinbashir1999@gmail.com>
2022-11-22 13:24:50 +0500

    fix: update page name

---
+++ pages/api/get-contracts.js
@@ -0,0 +1,29 @@
+import nextConnect from "next-connect";
+import withMongoose from "../../lib/mongoose/withMongoose";
+import { onError, onNoMatch } from "../../middlewares";
+import { authenticate } from "../../middlewares/auth";
+import Contract from "../../models/Contract";
+
+export default withMongoose(
+  nextConnect({
+    onError,
+    onNoMatch,
+  })
+    .use(authenticate)
+    .get(async (req, res, next) => {
+      const limit = Math.abs(req.query.limit) || 10;
+      const totalPages = await Contract.countDocuments();
+      const requestedPage = (Math.abs(req.query.page) || 1) - 1;
+      const page = requestedPage <= totalPages ? requestedPage : totalPages;
+
+      try {
+        const results = await Contract.find()
+          .limit(limit)
+          .skip(limit * page);
+
+        res.status(200).json(results);
+      } catch (err) {
+        next(err);
+      }
+    })
+);
